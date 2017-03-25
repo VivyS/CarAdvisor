d82e887a12433209a3b2da1c16f3e69801881eec
VivyS <visilve@yahoo.it>
2017-03-26 00:59:37 +0100

    Primo Commit versione dev di Matteo

---
+++ CarAdvisor/app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to "http://localhost:3000/"
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+end
